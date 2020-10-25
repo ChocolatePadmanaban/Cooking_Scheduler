9aabc1e9d3f81643348b76e3b08e0283aeea0809
ChocolatePadmanaban <pradeeppadmanaban7@gmail.com>
2020-10-25 22:23:48 +0530

    unique ingredient generator

---
+++ unique_ingredients_generator.py
@@ -0,0 +1,20 @@
+import csv
+import os
+
+ingredient_files=os.listdir("recipe_ingredients")
+ingredient_dict={}
+
+for ingredient_file in ingredient_files:
+    i_file = open('recipe_ingredients/'+ingredient_file)
+    csv_i_file = csv.reader(i_file)
+    for row in csv_i_file:
+        if row[0] not in ['name', 'question']:
+            if row[0] in ingredient_dict.keys():
+                if row[2] != ingredient_dict[row[0]]:
+                    ingredient_dict[row[0]]=row[2]
+            else:
+                ingredient_dict[row[0]]=row[2]
+
+row_format ="{:<20}" *2
+for key in sorted(ingredient_dict):
+    print( row_format.format(key, ingredient_dict[key]))
