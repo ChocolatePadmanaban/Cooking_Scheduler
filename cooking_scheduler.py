8dd8c86e52278a858002505ba498a045eeb70af0
ChocolatePadmanaban <pradeeppadmanaban7@gmail.com>
2020-10-31 16:55:55 +0530

    added factor

--- cooking_scheduler.py
+++ cooking_scheduler.py
@@ -1,7 +1,6 @@
 import csv
 import os
 
-
 ingredient_files=os.listdir("recipe_ingredients")
 recipes_dict={}
 
@@ -15,3 +14,17 @@ for i in enumerate(recipies, start=1):
 print()
 selected_recipe = int(input("Enter your option : ")) -1
 print("Selected option :", recipies[selected_recipe])
+
+with open('recipe_ingredients/'+recipies[selected_recipe]+'.csv') as i_file:
+    csv_i_file=csv.reader(i_file)
+    csv_i_file=[_ for _ in csv_i_file]
+    factor_num=float(input(csv_i_file[1][2]))
+    factor=factor_num/float(csv_i_file[1][1])
+    temp_dict={}
+    for row in csv_i_file:
+        if row[0] not in ['name','question']:
+            temp_dict[row[0]]=[float(row[1])*factor,row[2]]
+    recipes_dict[recipies[selected_recipe]]=temp_dict
+
+print(recipes_dict)
+
--- unique_ingredients_generator.py
+++ unique_ingredients_generator.py
@@ -1,6 +1,7 @@
 import csv
 import os
 
+
 ingredient_files=os.listdir("recipe_ingredients")
 ingredient_dict={}
 
@@ -9,13 +10,16 @@ for ingredient_file in ingredient_files:
     csv_i_file = csv.reader(i_file)
     for row in csv_i_file:
         if row[0] not in ['name', 'question']:
-            if row[0] in ingredient_dict.keys():
-                if row[2] != ingredient_dict[row[0]]:
-                    ingredient_dict[row[0]]=row[2]
+            if row[0] in ingredient_dict.keys() and row[2] != ingredient_dict[row[0]]:
+                if type(ingredient_dict[row[0]]) == type([]):
+                    ingredient_dict[row[0]].append(row[2])
+                else:
+                    ingredient_dict[row[0]]=[ingredient_dict[row[0]],row[2]]
             else:
                 ingredient_dict[row[0]]=row[2]
     i_file.close()
 
 row_format ="{:<20}" *2
 for key in sorted(ingredient_dict):
-    print( row_format.format(key, ingredient_dict[key]))
+    #print( row_format.format(key, ingredient_dict[key]))
+    print( key, ingredient_dict[key])
